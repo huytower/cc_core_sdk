2a2375e823a1f1d422770c2372db1498c0ea7be5
Tran Huy <huytower1990@gmail.com>
2026-05-22 21:59:05 +0700

    refactor ccsdk, apply inject

--- libraries/cc_sdk/lib/data/datasources/remote/cc_sdk_remote_data_source.dart
+++ libraries/cc_sdk/lib/data/datasources/remote/cc_sdk_remote_data_source.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+
 /// STEP 4: THE DATA SOURCE (The "Laborer")
 /// This is the lowest level. It talks directly to the server.
 /// It doesn't return "Failures", it throws "Exceptions".
@@ -11,4 +17,35 @@ abstract class CCSDKRemoteDataSource {
     bool useProxy,
     Duration? timeout,
   });
+}
+
+@LazySingleton(as: CCSDKRemoteDataSource)
+class CCSDKRemoteDataSourceImpl implements CCSDKRemoteDataSource {
+  final Dio dio;
+
+  const CCSDKRemoteDataSourceImpl({required this.dio});
+
+  @override
+  Future<String> executeCurlRequest({
+    required String url,
+    Map<String, String>? headers,
+    dynamic body,
+    required String method,
+    bool useProxy = false,
+    Duration? timeout,
+  }) async {
+    try {
+      final response = await dio.request(
+        url,
+        options: Options(
+          method: method,
+          headers: headers,
+        ),
+        data: body,
+      );
+      return jsonEncode(response.data);
+    } on DioException catch (e) {
+      throw HttpException(e.message ?? 'Network error');
+    }
+  }
 }
