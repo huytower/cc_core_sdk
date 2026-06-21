cd6714e0a577d22661270b97a0f24dcea63952c7
Tran Huy <huytower1990@gmail.com>
2026-06-21 11:10:53 +0700

    chore: update cc_core_sdk paths and repo metadata

--- cc_core_sdk/cc_sdk/lib/core/di/di.module.dart
+++ cc_core_sdk/cc_sdk/lib/core/di/di.module.dart
@@ -8,7 +8,7 @@ import 'dart:async' as _i687;
 import 'package:cc_sdk/core/di/module/cc_dependencies.dart' as _i1021;
 import 'package:cc_sdk/core/helper/cc_device_helper.dart' as _i918;
 import 'package:cc_sdk/core/helper/cc_device_info_helper.dart' as _i312;
-import 'package:cc_sdk/core/helper/cc_location_helper.dart' as _i542;
+import 'package:cc_sdk/core/helper/cc_location_helper.dart' as _i537;
 import 'package:cc_sdk/core/helper/cc_network_helper.dart' as _i548;
 import 'package:cc_sdk/core/helper/cc_responsive_helper.dart' as _i987;
 import 'package:cc_sdk/core/network/cc_network_info.dart' as _i13;
@@ -21,33 +21,28 @@ import 'package:internet_connection_checker_plus/internet_connection_checker_plu
 import 'package:shared_preferences/shared_preferences.dart' as _i460;
 
 class CcSdkPackageModule extends _i526.MicroPackageModule {
-  // initializes the registration of main-scope dependencies inside of GetIt
+// initializes the registration of main-scope dependencies inside of GetIt
   @override
   _i687.FutureOr<void> init(_i526.GetItHelper gh) async {
     final ccSdkDependencies = _$CcSdkDependencies();
     gh.factory<_i918.CcDeviceHelper>(() => _i918.CcDeviceHelper());
+    gh.factory<_i537.CcLocationHelper>(() => _i537.CcLocationHelper());
     gh.factory<_i987.CcResponsiveHelper>(() => _i987.CcResponsiveHelper());
-    gh.factory<_i542.CcLocationHelper>(() => _i542.CcLocationHelper());
     await gh.singletonAsync<_i460.SharedPreferences>(
       () => ccSdkDependencies.sharedPreferences,
       preResolve: true,
     );
     gh.singleton<_i161.InternetConnection>(
-      () => ccSdkDependencies.internetConnection,
-    );
+        () => ccSdkDependencies.internetConnection);
     gh.singleton<_i895.Connectivity>(() => ccSdkDependencies.connectivity);
     gh.singleton<_i833.DeviceInfoPlugin>(
-      () => ccSdkDependencies.deviceInfoPlugin,
-    );
+        () => ccSdkDependencies.deviceInfoPlugin);
     gh.singleton<_i13.CcNetworkInfo>(
-      () => _i13.CcNetworkInfoImpl(gh<_i895.Connectivity>()),
-    );
+        () => _i13.CcNetworkInfoImpl(gh<_i895.Connectivity>()));
     gh.singleton<_i548.CcNetworkHelper>(
-      () => _i548.CcNetworkHelper(gh<_i161.InternetConnection>()),
-    );
+        () => _i548.CcNetworkHelper(gh<_i161.InternetConnection>()));
     gh.lazySingleton<_i312.CcDeviceInfoHelper>(
-      () => _i312.CcDeviceInfoHelper(gh<_i833.DeviceInfoPlugin>()),
-    );
+        () => _i312.CcDeviceInfoHelper(gh<_i833.DeviceInfoPlugin>()));
     await gh.singletonAsync<_i739.CcDeviceEntity>(
       () => ccSdkDependencies.deviceModel(gh<_i312.CcDeviceInfoHelper>()),
       preResolve: true,
