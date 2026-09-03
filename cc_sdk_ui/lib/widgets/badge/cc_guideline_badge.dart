523b955bbfdc34ac734eae2b57011918504dea51
Tran Huy <huytower1990@gmail.com>
2026-09-03 17:44:38 +0700

    wip: bugfix: guideline

--- cc_sdk_ui/lib/widgets/badge/cc_guideline_badge.dart
+++ cc_sdk_ui/lib/widgets/badge/cc_guideline_badge.dart
@@ -178,7 +178,7 @@ class _CcGuidelineBadgeState extends State<CcGuidelineBadge>
         !widget.forceHideLabel) {
       labelWidget = ConstrainedBox(
         constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.3,
+          maxWidth: MediaQuery.of(context).size.width * 0.5,
         ),
         child: GestureDetector(
           onTap: widget.onLabelTap,
