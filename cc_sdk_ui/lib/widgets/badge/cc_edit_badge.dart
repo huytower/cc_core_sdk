8da20821c8b8c81adef627145d229e362768918b
Tran Huy <huytower1990@gmail.com>
2026-09-13 21:26:15 +0700

    bugfix: UI

---
+++ cc_sdk_ui/lib/widgets/badge/cc_edit_badge.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../export_cc_sdk_ui.dart';
+
+class CcEditBadge extends StatelessWidget {
+  const CcEditBadge({
+    super.key,
+    required this.icon,
+    required this.color,
+    required this.foregroundColor,
+    this.onTap,
+  });
+
+  final IconData icon;
+  final Color color;
+  final Color foregroundColor;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final scheme = context.ccColorScheme;
+    return CcBouncing(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(context.respDim(12)),
+      child: Container(
+        width: context.respDim(24),
+        height: context.respDim(24),
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          border: Border.all(color: scheme.surface, width: 1.5),
+          boxShadow: [
+            BoxShadow(color: scheme.shadow.withOpacity(0.3), blurRadius: 4),
+          ],
+        ),
+        child: Icon(
+          icon,
+          color: foregroundColor,
+          size: context.respIconSize(baseSize: 14),
+        ),
+      ),
+    );
+  }
+}
